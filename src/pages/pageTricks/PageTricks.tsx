6a6fba75a6e637ff71ad4747778a5ee43e6451c1
Cristian Romero <cristian.romero@softwareestrategico.com>
2022-01-15 15:11:11 -0500

    Create routers with bootstrap

--- src/pages/pageTricks/PageTricks.tsx
+++ src/pages/pageTricks/PageTricks.tsx
@@ -1,15 +1,15 @@
 import { Nav } from "react-bootstrap";
 import { Router } from "./Router";
+import { LinkContainer } from "react-router-bootstrap";
 import { PageTrickContainer } from "./styles";
 
 export const PageTricks = () => {
-  // const navigate = (path: string) => {
-  //   link(path);
-  // };
-
   return (
     <PageTrickContainer>
       <Nav defaultActiveKey="/debounce" className="flex-column">
+        <LinkContainer to="/debounce">
+          <Nav.Link>Debounce</Nav.Link>
+        </LinkContainer>
         <Nav.Link eventKey="debounce">Active</Nav.Link>
         <Nav.Link eventKey="link-1">Link</Nav.Link>
         <Nav.Link eventKey="link-2">Link</Nav.Link>
